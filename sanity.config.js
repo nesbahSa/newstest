e0ee555474f34a7b8ffa00b3be793c4e964906da
nesbahSa <admin@nesbah.net>
2025-01-13 20:31:34 +0700

    nyocokin sanity.config.js sama ounya nya riskai

--- sanity.config.js
+++ sanity.config.js
@@ -1,18 +1,17 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+
+'use client'
+
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { apiVersion, dataset, projectId } from './src/sanity/env'
+import { schema } from './src/sanity/schema'
 
 export default defineConfig({
-  name: 'default',
-  title: 'news_trial',
-
-  projectId: 'ap60nve6',
-  dataset: 'production',
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
-  },
+  basePath: '/studio',
+  projectId,
+  dataset,
+  schema,
+  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
 })
+
